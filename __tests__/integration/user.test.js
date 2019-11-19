0e2b372f660323fa9d1dc8df2a5213352f194803
Pablo Vinícius <vinicius.pablo.18@gmail.com>
2019-11-18 23:24:20 -0300

    adding factories logic

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -1,51 +1,54 @@
 import request from 'supertest';
 import app from '../../src/app';
 import truncate from '../util/truncate';
-import { User } from '../../src/app/models';
+import factory from '../factories';
 
 describe('User', () => {
-  const userOneData = {
-    name: 'Pablo Vinicius',
-    email: 'vinicius.pablo.18@gmail.com',
-    password: '123456',
-  };
-
   beforeEach(async () => {
     await truncate();
   });
 
   it('Should be able to register', async () => {
+    const user = await factory.attrs('User');
+
     const response = await request(app)
       .post('/users')
-      .send(userOneData);
+      .send(user);
 
     expect(response.body).toHaveProperty('user.id');
   });
 
   it('Should not be able to register because is missing a required argument', async () => {
+    const user = await factory.attrs('User');
+
     const response = await request(app)
       .post('/users')
-      .send({ email: userOneData.email, name: userOneData.name });
+      .send({ email: user.email, name: user.name });
 
     expect(response.body).toHaveProperty('error', 'Validation failed');
   });
 
   it('Should not be able to register a user with the same email', async () => {
+    const user = await factory.attrs('User');
+
     await request(app)
       .post('/users')
-      .send(userOneData);
+      .send(user);
 
     const response = await request(app)
       .post('/users')
-      .send(userOneData);
+      .send(user);
 
     expect(response.body).toHaveProperty('error', 'User already exists');
   });
 
   it('Should have its password encrypted when created', async () => {
-    const user = await User.create(userOneData);
+    const password = '123456';
+    const user = await factory.create('User', {
+      password,
+    });
 
-    const encrypted = await user.checkPassword(userOneData.password);
+    const encrypted = await user.checkPassword(password);
 
     expect(encrypted).toBe(true);
   });
