d7d64602f72ab6a2f9589be4aa12f3a37e9b4ad7
Pablo Vinícius <vinicius.pablo.18@gmail.com>
2019-12-24 10:29:25 -0300

    initial frontend configuration

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import GlobalStyles from './styles/global';
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyles />
+      <div className="App">
+        <h1>Initial Config</h1>
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ frontend/src/styles/global.js
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
+
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  body, input, button {
+    font: 14px Roboto, sans-serif;
+  }
+
+  #root {
+    min-height: 100vh;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
