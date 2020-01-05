0e69d94df498573b4b489d1fbf6e4bba8d4b897a
Pablo Vinícius <vinicius.pablo.18@gmail.com>
2020-01-05 12:10:31 -0300

    connecting plans list to api

--- frontend/src/pages/Plans/index.js
+++ frontend/src/pages/Plans/index.js
@@ -1,21 +1,47 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { Pagination, TableButton } from '~/components';
+import { Pagination, TableButton, Spinner } from '~/components';
 import { Container, BodyRow, Wrapper, Header } from './styles';
 
-const data = Array(10)
-  .fill(0)
-  .map((_, id) => ({
-    title: 'Diamond',
-    duration: id + 1,
-    value: 'R$ 130,00',
-    id: `${id}`,
-  }));
-const numberOfPages = 10;
-const currentPage = 1;
-const onPageChange = () => {};
+import api from '~/services/api';
 
 const Plans = () => {
+  const [page, setPage] = useState(1);
+  const [data, setData] = useState([]);
+  const [pagesCount, setPagesCount] = useState(0);
+  const [loading, setLoading] = useState(false);
+
+  const getPlans = async () => {
+    setLoading(true);
+    try {
+      const response = await api.get('plans', {
+        params: {
+          page: page - 1,
+          perPage: 10,
+        },
+      });
+
+      setData(response.data.plans);
+      setPagesCount(response.data.pages);
+      setLoading(false);
+    } catch (err) {
+      setData([]);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getPlans();
+  }, [page]);
+
+  const handleDelete = id => {
+    const confirmed = window.confirm('Tem certeza que deseja deletar?');
+    if (confirmed) {
+      alert(`Delete: ${id}`);
+    }
+  };
+
   return (
     <Wrapper>
       <Header>
@@ -26,38 +52,44 @@ const Plans = () => {
           </TableButton>
         </Link>
       </Header>
-      <Container>
-        <thead>
-          <tr>
-            <th>TÍTULO</th>
-            <th>DURAÇÃO</th>
-            <th>VALOR p/ MÊS</th>
-            <th />
-          </tr>
-        </thead>
-        <tbody>
-          {data.map(row => (
-            <BodyRow key={row.id}>
-              <td>{row.title}</td>
-              <td>
-                {row?.duration > 1
-                  ? `${row?.duration} meses`
-                  : `${row?.duration} mês`}
-              </td>
-              <td>{row.value}</td>
-              <td>
-                <Link to={`/plans/${row.id}`}>editar</Link>
-                <span onClick={() => {}}>apagar</span>
-              </td>
-            </BodyRow>
-          ))}
-        </tbody>
-      </Container>
-      <Pagination
-        numberOfPages={numberOfPages}
-        currentPage={currentPage}
-        onPageClick={onPageChange}
-      />
+      {loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <Container>
+            <thead>
+              <tr>
+                <th>TÍTULO</th>
+                <th>DURAÇÃO</th>
+                <th>VALOR p/ MÊS</th>
+                <th />
+              </tr>
+            </thead>
+            <tbody>
+              {data.map(row => (
+                <BodyRow key={row.id}>
+                  <td>{row.title}</td>
+                  <td>
+                    {row?.duration > 1
+                      ? `${row?.duration} meses`
+                      : `${row?.duration} mês`}
+                  </td>
+                  <td>{row.textualPrice}</td>
+                  <td>
+                    <Link to={`/plans/${row.id}`}>editar</Link>
+                    <span onClick={() => handleDelete(row.id)}>apagar</span>
+                  </td>
+                </BodyRow>
+              ))}
+            </tbody>
+          </Container>
+          <Pagination
+            numberOfPages={pagesCount}
+            currentPage={page}
+            onPageClick={value => setPage(value)}
+          />
+        </>
+      )}
     </Wrapper>
   );
 };
