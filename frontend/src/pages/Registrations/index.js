5414370d54f422dda606107c5d8f51a67d7d41b8
Pablo Vinícius <vinicius.pablo.18@gmail.com>
2020-01-05 15:51:18 -0300

    connecting registrations table to api

--- frontend/src/pages/Registrations/index.js
+++ frontend/src/pages/Registrations/index.js
@@ -1,23 +1,63 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react';
+import pt from 'date-fns/locale/pt';
+import { MdCheckCircle } from 'react-icons/md';
+import { format, parseISO } from 'date-fns';
+import { toast } from 'react-toastify';
 import { Link } from 'react-router-dom';
-import { Pagination, TableButton } from '~/components';
+import { Pagination, TableButton, Spinner } from '~/components';
 import { Container, BodyRow, Wrapper, Header } from './styles';
 
-const data = Array(10)
-  .fill(0)
-  .map((_, id) => ({
-    student: 'João José',
-    plan: 'Standard',
-    begin: '25 de setembro de 2005',
-    end: '26 de setembro de 2005',
-    active: id % 2 === 0,
-    id: `${id}`,
-  }));
-const numberOfPages = 10;
-const currentPage = 1;
-const onPageChange = () => {};
+import api from '~/services/api';
 
 const Registrations = () => {
+  const [page, setPage] = useState(1);
+  const [data, setData] = useState([]);
+  const [pagesCount, setPagesCount] = useState(0);
+  const [loading, setLoading] = useState(false);
+
+  const getRegistrations = async () => {
+    setLoading(true);
+    try {
+      const response = await api.get('registrations', {
+        params: {
+          page: page - 1,
+          perPage: 10,
+        },
+      });
+
+      setData(response.data.registrations);
+      setPagesCount(response.data.pages);
+      setLoading(false);
+    } catch (err) {
+      setData([]);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getRegistrations();
+  }, [page]);
+
+  const handleDelete = async id => {
+    const confirmed = window.confirm('Tem certeza que deseja deletar?');
+    if (confirmed) {
+      try {
+        await api.delete(`registrations/${id}`);
+        toast.success('Plano deletado com sucesso!');
+        getRegistrations();
+      } catch (err) {
+        toast.error('Erro ao deletar plano');
+      }
+    }
+  };
+
+  const formatDate = iso => {
+    return format(parseISO(iso), "dd 'de' MMMM 'de' yyyy", {
+      locale: pt,
+    });
+  };
+
   return (
     <Wrapper>
       <Header>
@@ -28,38 +68,49 @@ const Registrations = () => {
           </TableButton>
         </Link>
       </Header>
-      <Container>
-        <thead>
-          <tr>
-            <th>ALUNO</th>
-            <th>PLANO</th>
-            <th>INÍCIO</th>
-            <th>TÉRMINO</th>
-            <th>ATIVA</th>
-            <th />
-          </tr>
-        </thead>
-        <tbody>
-          {data.map(row => (
-            <BodyRow key={row.id}>
-              <td>{row.student}</td>
-              <td>{row.plan}</td>
-              <td>{row.begin}</td>
-              <td>{row.end}</td>
-              <td>{row.active ? 'Sim' : 'Não'}</td>
-              <td>
-                <Link to={`/registrations/${row.id}`}>editar</Link>
-                <span onClick={() => {}}>apagar</span>
-              </td>
-            </BodyRow>
-          ))}
-        </tbody>
-      </Container>
-      <Pagination
-        numberOfPages={numberOfPages}
-        currentPage={currentPage}
-        onPageClick={onPageChange}
-      />
+      {loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <Container>
+            <thead>
+              <tr>
+                <th>ALUNO</th>
+                <th>PLANO</th>
+                <th>INÍCIO</th>
+                <th>TÉRMINO</th>
+                <th>ATIVA</th>
+                <th />
+              </tr>
+            </thead>
+            <tbody>
+              {data.map(row => (
+                <BodyRow key={row.id}>
+                  <td>{row.student.name}</td>
+                  <td>{row.plan.title}</td>
+                  <td>{formatDate(row.start_date)}</td>
+                  <td>{formatDate(row.end_date)}</td>
+                  <td>
+                    <MdCheckCircle
+                      color={row.active ? '#42cb59' : '#ddd'}
+                      size={20}
+                    />
+                  </td>
+                  <td>
+                    <Link to={`/registrations/${row.id}`}>editar</Link>
+                    <span onClick={() => handleDelete(row.id)}>apagar</span>
+                  </td>
+                </BodyRow>
+              ))}
+            </tbody>
+          </Container>
+          <Pagination
+            numberOfPages={pagesCount}
+            currentPage={page}
+            onPageClick={value => setPage(value)}
+          />
+        </>
+      )}
     </Wrapper>
   );
 };
