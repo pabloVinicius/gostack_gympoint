76e5b71e3efeac2404340eb0f79013dc0860403a
Pablo Vinícius <vinicius.pablo.18@gmail.com>
2020-01-05 09:59:12 -0300

    setting up redux, redux-saga and reactotron

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
-
 import './config/ReactotronConfig';
 import GlobalStyles from './styles/global';
 import Routes from './routes';
 import history from './services/history';
+import store from './store';
 
 const App = () => {
   return (
-    <Router history={history}>
-      <Routes />
-      <GlobalStyles />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <Routes />
+        <GlobalStyles />
+      </Router>
+    </Provider>
   );
 };
 
